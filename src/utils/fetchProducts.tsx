41733ec9253f1d112b74531e1e769e576636dd93
Holly Skinner <hollyskinner97@gmail.com>
2025-04-28 15:07:17 +0100

    create fetchProducts util

---
+++ src/utils/fetchProducts.tsx
@@ -0,0 +1,14 @@
+import { Product } from "../types/product";
+
+export default async function fetchProducts(): Promise<Product[]> {
+  try {
+    const response = await fetch("https://fakestoreapi.com/products");
+    if (!response.ok) {
+      throw new Error("Failed fetching products");
+    }
+    const products: Product[] = await response.json();
+    return products;
+  } catch (error) {
+    console.error("Error fetching products:", error);
+  }
+}
